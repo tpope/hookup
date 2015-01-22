ab4b0e638f47e7d8ba46ad52eb66ee5c433adc9a
Bob Lail <bob.lail@cph.org>
2015-01-21 20:02:03 -0600

    [refactor] Refactored PostCheckout to shell out fewer times to `git diff --name-status` (20m)

--- lib/hookup/post_checkout.rb
+++ lib/hookup/post_checkout.rb
@@ -1,7 +1,9 @@
+require "delegate"
+
 class Hookup
   class PostCheckout
 
-    attr_reader :old, :new, :env
+    attr_reader :old_sha, :new_sha, :env
 
     def partial?
       @partial
@@ -37,13 +39,13 @@ class Hookup
       end
       opts.parse!(args)
 
-      @old = args.shift
-      if @old == '0000000000000000000000000000000000000000'
-        @old = EMPTY_DIR
-      elsif @old.nil?
-        @old = '@{-1}'
+      @old_sha = args.shift
+      if @old_sha == '0000000000000000000000000000000000000000'
+        @old_sha = EMPTY_DIR
+      elsif @old_sha.nil?
+        @old_sha = '@{-1}'
       end
-      @new = args.shift || 'HEAD'
+      @new_sha = args.shift || 'HEAD'
       @partial = (args.shift == '0')
 
       env['HOOKUP_SCHEMA_DIR'] = 'db' unless env['HOOKUP_SCHEMA_DIR'] && File.directory?(schema_dir)
@@ -63,7 +65,7 @@ class Hookup
 
     def bundle
       return unless bundler?
-      if x("git diff --name-only #{old} #{new}") =~ /^Gemfile|\.gemspec$/
+      if changes.grep(/^Gemfile|\.gemspec$/).any?
         begin
           # If Bundler in turn spawns Git, it can get confused by $GIT_DIR
           git_dir = ENV.delete('GIT_DIR')
@@ -80,38 +82,39 @@ class Hookup
 
     def migrate
       schemas = possible_schemas.select do |schema|
-        status = x("git diff --name-status #{old} #{new} -- #{schema}").chomp
-        rake 'db:create' if status =~ /^A/
-        status !~ /^D/ && !status.empty?
+        change = changes[schema]
+        rake 'db:create' if change && change.added?
+        change && !change.deleted?
       end
 
       return if schemas.empty?
 
-      migrations = x("git diff --name-status #{old} #{new} -- #{schema_dir}/migrate").scan(/.+/).map {|l| l.split(/\t/) }
+      migrations = changes.grep(/^#{schema_dir}\/migrate/)
       begin
-        migrations.select {|(t,f)| %w(D M).include?(t)}.reverse.each do |type, file|
+        migrations.select { |migration| migration.deleted? || migration.modified? }.reverse.each do |migration|
+          file = migration.file
           begin
-            system 'git', 'checkout', old, '--', file
+            system 'git', 'checkout', old_sha, '--', file
             unless rake 'db:migrate:down', "VERSION=#{File.basename(file)}"
               raise Error, "Failed to rollback #{File.basename(file)}"
             end
           ensure
-            if type == 'D'
+            if migration.deleted?
               system 'git', 'rm', '--force', '--quiet', '--', file
             else
-              system 'git', 'checkout', new, '--', file
+              system 'git', 'checkout', new_sha, '--', file
             end
           end
         end
 
-        if migrations.any? {|(t,f)| %w(A M).include?(t)}
+        if migrations.any? { |migration| migration.added? || migration.modified? }
           rake 'db:migrate'
         end
 
       ensure
-        changes = x("git diff --name-status #{new} -- #{schemas.join(' ')}")
+        _changes = x("git diff --name-status #{new_sha} -- #{schemas.join(' ')}")
 
-        unless changes.empty?
+        unless _changes.empty?
           system 'git', 'checkout', '--', *schemas
 
           puts "Schema out of sync."
@@ -142,13 +145,65 @@ class Hookup
     end
 
     def system(*args)
-      puts "\e[90m#{args.join(" ")}\e[0m"
+      puts "\e[90m[#{File.basename Dir.pwd}] #{args.join(" ")}\e[0m"
       super
     end
 
     def x(command)
-      puts "\e[90m#{command}\e[0m"
+      puts "\e[90m[#{File.basename Dir.pwd}] #{command}\e[0m"
       %x{#{command}}
+    end
+
+    def changes
+      @changes ||= DiffChanges.new((old_sha != new_sha) && x("git diff --name-status #{old_sha} #{new_sha}"))
+    end
+
+    class DiffChange < Struct.new(:type, :file)
+      def added?
+        type == "A"
+      end
+
+      def copied?
+        type == "C"
+      end
+
+      def deleted?
+        type == "D"
+      end
+
+      def modified?
+        type == "M"
+      end
+
+      def renamed?
+        type == "R"
+      end
+
+      def type_changed?
+        type == "T"
+      end
+
+      def unmerged?
+        type == "U"
+      end
+
+      def broken?
+        type == "B"
+      end
+    end
+
+    class DiffChanges < SimpleDelegator
+      def initialize(diff)
+        super diff.to_s.scan(/^([^\t]+)\t(.*)$/).map { |(type, file)| DiffChange.new(type, file) }
+      end
+
+      def grep(regex)
+        __getobj__.select { |change| change.file =~ regex }
+      end
+
+      def [](filename)
+        __getobj__.detect { |change| change.file == filename }
+      end
     end
 
   end
