226e623605baac6c5b1be8c2257d2a562120c339
Bob Lail <bob.lail@cph.org>
2015-06-05 17:51:22 -0500

    [skip] Hid output from `bundle check` (1m)

--- lib/hookup/post_checkout.rb
+++ lib/hookup/post_checkout.rb
@@ -76,7 +76,7 @@ class Hookup
         begin
           # If Bundler in turn spawns Git, it can get confused by $GIT_DIR
           git_dir = ENV.delete('GIT_DIR')
-          unless system("bundle check")
+          unless system("bundle check > /dev/null 2> /dev/null")
             Dir.chdir(working_dir) do
               system("bundle | grep -v '^Using ' | grep -v ' is complete'")
             end
